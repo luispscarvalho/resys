243773dca15af8a3b782eacb262a79bc0b1e6754
luispscarvalho <luispscarvalho@gmail.com>
2017-08-15 22:53:16 -0300

    Refactoring + Template were moved to other packages

---
+++ src/br/org/resys/rre/impl/Template.java
@@ -0,0 +1,39 @@
+package br.org.resys.rre.impl;
+
+import br.org.resys.rre.ITemplate;
+
+public class Template implements ITemplate {
+
+	private String before;
+	private String after;
+
+	@Override
+	public ITemplate setBefore(String before) {
+		this.before = before;
+
+		return this;
+	}
+
+	@Override
+	public String getBefore() {
+		return before;
+	}
+
+	@Override
+	public ITemplate setAfter(String after) {
+		this.after = after;
+
+		return this;
+	}
+
+	@Override
+	public String getAfter() {
+		return after;
+	}
+
+	@Override
+	public String toJson() {
+		return "{before: '" + before + "', after: '" + after + "'}";
+	}
+
+}
