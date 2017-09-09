f6c485e679d77760923c8808c963b785641b69fc
luispscarvalho <luispscarvalho@gmail.com>
2017-09-08 22:23:19 -0300

    + ontoprefix
    helpful as new individuals are added to ontologies

--- src/br/org/resys/en/OntosIRI.java
+++ src/br/org/resys/en/OntosIRI.java
@@ -1,5 +1,8 @@
 package br.org.resys.en;
 
+import org.semanticweb.owlapi.model.PrefixManager;
+import org.semanticweb.owlapi.util.DefaultPrefixManager;
+
 /**
  * IRIs of all ontologies.
  * 
@@ -7,19 +10,36 @@ package br.org.resys.en;
  */
 public enum OntosIRI {
 
-	ONTOS_IRI("http://www.semanticweb.org/resys/ontologies"), REPOSITORIES_IRI(
-			ONTOS_IRI.getIri() + "/2016/1/repositories"), SMELLS_IRI(
-					ONTOS_IRI.getIri() + "/2016/1/codesmells"), METRICS_IRI(
-							ONTOS_IRI.getIri() + "/2016/1/metrics"), OSORE_IRI(ONTOS_IRI.getIri() + "/2017/4/osore");
+	ONTOS_IRI("http://www.semanticweb.org/resys/ontologies", null), REPOSITORIES_IRI(
+			ONTOS_IRI.getIri() + "/2016/1/repositories",
+			new DefaultPrefixManager("http://www.semanticweb.org/resys/ontologies/2016/1/repositories#")), SMELLS_IRI(
+					ONTOS_IRI.getIri() + "/2016/1/codesmells",
+					new DefaultPrefixManager(
+							"http://www.semanticweb.org/resys/ontologies/2016/1/codesmells#")), METRICS_IRI(
+									ONTOS_IRI.getIri() + "/2016/1/metrics",
+									new DefaultPrefixManager(
+											"http://www.semanticweb.org/resys/ontologies/2016/1/metrics#")), OSORE_IRI(
+													ONTOS_IRI.getIri() + "/2017/4/osore",
+													new DefaultPrefixManager(
+															"http://www.semanticweb.org/resys/ontologies/2017/4/osore#")), OCEAN_IRI(
+																	ONTOS_IRI.getIri() + "/2016/2/ocean",
+																	new DefaultPrefixManager(
+																			"http://www.semanticweb.org/resys/ontologies/2016/2/ocean#"));
 
 	private String iri;
+	private PrefixManager prefix;
 
-	private OntosIRI(String iri) {
+	private OntosIRI(String iri, PrefixManager prefix) {
 		this.iri = iri;
+		this.prefix = prefix;
 	}
 
 	public String getIri() {
 		return iri;
+	}
+
+	public PrefixManager getPrefix() {
+		return prefix;
 	}
 
 }
